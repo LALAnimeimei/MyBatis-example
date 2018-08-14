b835101d267f5cb55037e65039357d9d707ebe65
390766 <529350056@qq.com>
2018-08-14 15:27:21 +0800

    配置学习logback日志

--- src/main/java/com/mybatis/controller/RestController.java
+++ src/main/java/com/mybatis/controller/RestController.java
@@ -1,7 +1,6 @@
 package com.mybatis.controller;
 
 import com.mybatis.entity.User;
-import com.mybatis.mapper.OrderMapper;
 import com.mybatis.mapper.UserMapper;
 import com.mybatis.service.OrderService;
 import com.mybatis.service.UserService;
@@ -9,10 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 
 @Controller
--- src/main/java/com/mybatis/service/OrderService.java
+++ src/main/java/com/mybatis/service/OrderService.java
@@ -1,7 +1,10 @@
 package com.mybatis.service;
 
+
 import com.mybatis.entity.orderdetail;
 import com.mybatis.mapper.OrderMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,11 +15,15 @@ import java.util.List;
 @Service
 @Transactional
 public class OrderService {
+    private Logger logger= LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private OrderMapper orderMapper;
 
     public List<orderdetail> OrderToUser(){
         List list=orderMapper.OrderToUser();
+        logger.info("你好info");
+        logger.debug("你好debug");
         System.out.println(list);
         return list;
     }
