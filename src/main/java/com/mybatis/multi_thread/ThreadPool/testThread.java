419831bf4772fcfef9160401ffc5a7e86945f026
390766 <529350056@qq.com>
2018-08-22 11:04:47 +0800

    多线程编程的一些测试用例

---
+++ src/main/java/com/mybatis/multi_thread/ThreadPool/testThread.java
@@ -0,0 +1,15 @@
+package com.mybatis.multi_thread.ThreadPool;
+
+public class testThread implements Runnable{
+    @Override
+    public void run(){
+        try
+        {
+            Thread.sleep(300);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/mybatis/multi_thread/VolatileTest/Run.java
@@ -0,0 +1,16 @@
+package com.mybatis.multi_thread.VolatileTest;
+
+public class Run {
+    public static void main(String[] args){
+//        System.out.println(System.getenv());
+        try{
+            RunThread thread=new RunThread();
+            thread.start();
+            Thread.sleep(1000);
+            thread.setRunning(false);
+            System.out.println("已经赋值为FALSE了");
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
